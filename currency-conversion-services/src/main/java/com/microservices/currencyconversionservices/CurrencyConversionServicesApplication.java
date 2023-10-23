445a62bd03769fc4dd35e004f17190ce3926d847
Parvathi Chinthalapati <parvathi.rvrjc@gmail.com>
2023-10-23 20:58:24 +0530

    Microservices

---
+++ currency-conversion-services/src/main/java/com/microservices/currencyconversionservices/CurrencyConversionServicesApplication.java
@@ -0,0 +1,16 @@
+package com.microservices.currencyconversionservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableFeignClients
+public class CurrencyConversionServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServicesApplication.class, args);
+	}
+
+}
---
+++ limits-service/src/main/java/com/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,17 @@
+package com.microservices.limitsservice.controller;
+
+import com.microservices.limitsservice.bean.Limits;
+import com.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+    @Autowired
+    public Configuration configuration;
+    @GetMapping("/limits")
+    public Limits retreiveLimits(){
+        return new Limits(configuration.getMin(),configuration.getMax());
+    }
+}
