71e5e2e2f459537fd8f39c42c69f32972ed36bb7
Parvathi Chinthalapati <parvathi.rvrjc@gmail.com>
2023-11-16 15:48:52 +0530

    Updating currency-exchange service

--- currency-conversion-services/src/main/java/com/microservices/currencyconversionservices/CurrencyConversionController.java
+++ currency-conversion-services/src/main/java/com/microservices/currencyconversionservices/CurrencyConversionController.java
@@ -4,17 +4,32 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration(proxyBeanMethods = false)
+class RestTemplateConfiguration {
+
+	@Bean
+	RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+}
+
 @RestController
 public class CurrencyConversionController {
-	
+
 	@Autowired
 	private CurrencyExchangeProxy proxy;
+
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to,
@@ -22,23 +37,24 @@ public class CurrencyConversionController {
 		HashMap<String, String> uriVariables = new HashMap<>();
 		uriVariables.put("from", from);
 		uriVariables.put("to", to);
-		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
-				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class,
-				uriVariables);
-		System.out.println("response "+responseEntity);
+		ResponseEntity<CurrencyConversion> responseEntity = restTemplate.getForEntity(
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+		System.out.println("response " + responseEntity);
 		CurrencyConversion currencyConversion = responseEntity.getBody();
 		return new CurrencyConversion(1000L, from, to, quantity, currencyConversion.getConversionMultiple(),
-				quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment()+" rest");
+				quantity.multiply(currencyConversion.getConversionMultiple()),
+				currencyConversion.getEnvironment() + " rest");
 
 	}
-	
+
 	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
 
 		CurrencyConversion currencyConversion = proxy.retreiveExchangeValue(from, to);
 		return new CurrencyConversion(1000L, from, to, quantity, currencyConversion.getConversionMultiple(),
-				quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment()+" feign");
+				quantity.multiply(currencyConversion.getConversionMultiple()),
+				currencyConversion.getEnvironment() + " feign");
 
 	}
 }
--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
 	@Autowired
 	private Environment environment;
 
@@ -17,10 +21,10 @@ public class CurrencyExchangeController {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retreiveExchangeValue(@PathVariable String from, @PathVariable String to) {
-
+		logger.info("retreive exchange value called with {} to {} " + from, to);
 		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
-		if(currencyExchange == null)
-			throw new RuntimeException("Unable to find data from = "+from+" to "+to);
+		if (currencyExchange == null)
+			throw new RuntimeException("Unable to find data from = " + from + " to " + to);
 
 		String port = environment.getProperty("local.server.port");
 		currencyExchange.setEnvironment(port);
